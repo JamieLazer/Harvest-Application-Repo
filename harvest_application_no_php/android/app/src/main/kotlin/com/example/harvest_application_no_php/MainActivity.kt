3f5ba1f4a43d7379fc9ead5195434c27ac2ac330
JamieLazer <jamielazenby9793@gmail.com>
2023-03-20 18:53:29 +0200

    Feature added: User sign up and login

---
+++ harvest_application_no_php/android/app/src/main/kotlin/com/example/harvest_application_no_php/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.harvest_application_no_php
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ harvest_application_no_php/lib/views/ConnectionSettings.dart
@@ -0,0 +1,10 @@
+import 'package:mysql1/mysql1.dart';
+
+//These are the database details
+var settings = ConnectionSettings(
+  host: 'db4free.net', 
+  port: 3306,
+  user: 'hardcoded',
+  password: '5Scrummies@SD',
+  db: 'harvestapp'
+);
