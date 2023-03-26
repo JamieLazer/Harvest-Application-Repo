8b1df9a0fe4374c52f171d0d6d3406b67c27bf85
melo <127689966+avocadotortilla@users.noreply.github.com>
2023-03-26 21:06:49 +0200

    fonts, styles and welcome page

---
+++ harvest_application_no_php/lib/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const TextStyle welcomePageText = TextStyle(
+  fontSize: 20,
+  color: Colors.white,
+  decoration: TextDecoration.none,
+  fontFamily: 'AbeeZee',
+  fontWeight: FontWeight.normal,
+);
+
+//app colours
+Color primaryColour = const Color(0xFF80A87A);
+Color secondaryColour = const Color(0xFF4E7449);
+Color tertiaryColour = const Color(0xFFDCF9D7);
