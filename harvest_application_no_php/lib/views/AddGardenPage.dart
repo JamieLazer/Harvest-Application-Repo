e60c2361891609ce840123a78fbdec7f8ff65b1f
melo <127689966+avocadotortilla@users.noreply.github.com>
2023-03-31 20:55:15 +0200

    styled addgardenpage

--- harvest_application_no_php/lib/views/AddGardenPage.dart
+++ harvest_application_no_php/lib/views/AddGardenPage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:dartfactory/styles.dart';
 import 'package:mysql1/mysql1.dart';
 import 'package:flutter/material.dart';
 
@@ -21,12 +22,52 @@ class AddGardenPage extends StatelessWidget {
 
     //When you push a new screen after a MaterialApp, a back button is automatically added
     return Scaffold(
+      backgroundColor: Colors.white,
       //This is the title at the top of the screen
       appBar: AppBar(
-        title: const Text('Harvest'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: primaryColour, // Set the color of the back button
+          onPressed: () => Navigator.of(context).pop(),
+        ),
       ),
       //The body is filled with the AddGardenForm class below
-      body: AddGardenForm(userID, gardens),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 2,
+            color: Colors.white,
+            
+          ),
+          Container(
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height / 2),
+            height: MediaQuery.of(context).size.height / 2,
+            color: primaryColour,
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: 400,
+                width: 350,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+                child: Center(
+                  child: AddGardenForm(userID, gardens)
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -70,67 +111,97 @@ class _AddGardenFormState extends State<AddGardenForm> {
   // Everything below determines how the page is displayed
   Widget build(BuildContext context) {
     //we are using a form to allow for input validation
-    return Form(
-        key: _formKey,
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Add a Garden',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                controller: gardenController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Garden Name',
-                ),
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'This field cannot be empty';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            Container(
-                height: 60,
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: ElevatedButton(
-                    child: const Text('Add Garden',
-                        style: TextStyle(fontSize: 20)),
-                    onPressed: () async {
-                      //Validate returns true if the form is valid, or false otherwise.
-                      if (_formKey.currentState!.validate()) {
-                        //If the form is valid
-                        //Establish connection with the database
-                        var conn = await MySqlConnection.connect(settings);
-                        String gardenName = gardenController.text;
-                        //Add the garden to the LOG table in the database
-                        await conn.query(
-                            'CALL ADD_GARDEN(?, ?)', [userID, gardenName]);
+    return Center(
+        child: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'Create a New Garden!',
+                      style: blackText.copyWith(fontSize: 25),
+                    )
+                    ),
 
-                        //Request the updated users gardens from the database
-                        var updatedGardens = await conn.query(
-                            'select * from LOG where USER_ID = ?', [userID]);
-                        //Convert the results of the database query to a list
-                        List updatedGardensList = updatedGardens.toList();
-
-                        //Create the arguments that we will pass to the next page
-                        //The arguments we pass to a new page can be any object
-                        UserInfoArguments args =
-                            UserInfoArguments(userID, updatedGardensList);
-
-                        //Navigate back to the user garden screen using a named route and pass the new page the arguments
-                        Navigator.pushNamed(context, '/userGardens', arguments: args);
+                    SizedBox(height: 60,),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: gardenController,
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black54
+                        )
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black54)),
+                      errorBorder: UnderlineInputBorder(),
+                      labelText: 'Garden Name',
+                      labelStyle: loginPageText.copyWith(
+                        fontSize: 14,
+                        color: Colors.black54
+                      )
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'This field cannot be empty';
                       }
-                    })),
-          ],
-        ));
+                      return null;
+                    },
+                  ),
+                ),
+                Container(
+                    height: 60,
+                    alignment: Alignment.bottomRight,
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: tertiaryColour,
+                        ),
+                        child: Text('Add Garden',
+                            style: secondaryColourText.copyWith(
+                              fontSize: 16
+                            ),
+                            ),
+                        onPressed: () async {
+                          //Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            //If the form is valid
+                            //Establish connection with the database
+                            var conn = await MySqlConnection.connect(settings);
+                            String gardenName = gardenController.text;
+                            //Add the garden to the LOG table in the database
+                            await conn.query(
+                                'CALL ADD_GARDEN(?, ?)', [userID, gardenName]);
+
+                            //Request the updated users gardens from the database
+                            var updatedGardens = await conn.query(
+                                'select * from LOG where USER_ID = ?',
+                                [userID]);
+                            //Convert the results of the database query to a list
+                            List updatedGardensList = updatedGardens.toList();
+
+                            //Create the arguments that we will pass to the next page
+                            //The arguments we pass to a new page can be any object
+                            UserInfoArguments args =
+                                UserInfoArguments(userID, updatedGardensList);
+
+                            //Navigate back to the user garden screen using a named route and pass the new page the arguments
+                            Navigator.pushNamed(context, '/userGardens',
+                                arguments: args);
+                          }
+                        })),
+              ],
+            ),
+          )
+          )
+        );
   }
 }
