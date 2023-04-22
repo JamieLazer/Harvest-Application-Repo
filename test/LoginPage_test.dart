051c1155e9fc4fab53c299cd1a0d41854f79251a
Khayelihle Akil Mabusela <khayelihle.mabusela@gmail.com>
2023-04-22 18:42:46 +0200

    More LoginPage tests.

--- test/LoginPage_test.dart
+++ test/LoginPage_test.dart
@@ -1,6 +1,10 @@
 import 'package:dartfactory/views/LoginPage.dart';
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:mysql1/mysql1.dart';
+
+class MockBuildContext extends Mock implements BuildContext {}
 
 void main() {
   test('getConnection() returns a MySqlConnection', () async {
@@ -13,8 +17,8 @@ void main() {
 
   test('Test login with correct email and password', () async {
     // Arrange
-    final email = 'unittest@gmail.com';
-    final password = 'test123';
+    const email = 'unittest@gmail.com';
+    const password = 'test123';
     final conn = await MySqlConnection.connect(ConnectionSettings(
         host: 'db4free.net',
         port: 3306,
@@ -42,8 +46,8 @@ void main() {
 
   test('Test login with incorrect email or password', () async {
     // Arrange
-    final email = 'unittest@gmail.com';
-    final password = 'Unitest';
+    const email = 'unittest@gmail.com';
+    const password = 'Unitest';
     final conn = await MySqlConnection.connect(ConnectionSettings(
         host: 'db4free.net',
         port: 3306,
@@ -69,5 +73,66 @@ void main() {
         'DELETE FROM USERS WHERE user_email = ? AND user_password = ?',
         [email, password]);
     await conn.close();
+  });
+  group('Login page widgets', () {
+    testWidgets('buildEmail widget', (WidgetTester tester) async {
+      final emailController = TextEditingController();
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Form(
+              child: buildEmail(emailController),
+            ),
+          ),
+        ),
+      );
+      // check if the email label is present
+      expect(find.text('Email'), findsOneWidget);
+
+      // enter text in the email field
+      await tester.enterText(find.byType(TextFormField), 'email@test.com');
+      expect(emailController.text, 'email@test.com');
+    });
+
+    testWidgets('buildPassword widget', (WidgetTester tester) async {
+      final passwordController = TextEditingController();
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Form(
+              child: buildPassword(passwordController),
+            ),
+          ),
+        ),
+      );
+
+      // check if the password label is present
+      expect(find.text('Password'), findsOneWidget);
+
+      // enter text in the password field
+      await tester.enterText(find.byType(TextFormField), 'testpassword');
+      expect(passwordController.text, 'testpassword');
+    });
+
+    testWidgets('buildLoginButton widget', (WidgetTester tester) async {
+      final emailController = TextEditingController();
+      final passwordController = TextEditingController();
+      final formKey = GlobalKey<FormState>();
+      final BuildContext context = MockBuildContext();
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Form(
+              key: formKey,
+              child: buildLoginButton(
+                  emailController, passwordController, formKey, context),
+            ),
+          ),
+        ),
+      );
+
+      // check if the login button is present
+      expect(find.text('log in'), findsOneWidget);
+    });
   });
 }
