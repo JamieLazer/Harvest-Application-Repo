800b8699edce8ad4106a1391b46b1b137d698ed7
Khayelihle Akil Mabusela <khayelihle.mabusela@gmail.com>
2023-04-22 18:05:58 +0200

    Added a test to AddFoodPage_test.dart to see if error message pops up if input is empty.

---
+++ test/AddFoodPage_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:dartfactory/views/AddFoodPage.dart';
+
+// Mock NavigatorObserver
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+void main() {
+    testWidgets('should show error when input is empty', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: AddFoodForm(1, 2, const [], ''),
+          ),
+        ),
+      );
+
+      // Verify that validator is working
+      expect(find.text('This field cannot be empty'), findsNothing);
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pump();
+      expect(find.text('This field cannot be empty'), findsOneWidget);
+    });
+}
