260d05ea7a32a032c9e50121626cc5328b922ba3
Jamie Lazenby <126279661+JamieLazer@users.noreply.github.com>
2023-05-08 13:41:31 +0200

    Update PieData_test.dart

--- test/PieData_test.dart
+++ test/PieData_test.dart
@@ -3,8 +3,8 @@ import 'package:dartfactory/PieData.dart';
 
 void main() {
   test('PieData constructor initializes name and yield', () {
-    final pieData = PieData('Apple', 0.75);
+    final pieData = PieData('Apple', 0.80);
     expect(pieData.name, 'Apple');
-    expect(pieData.yield, 0.75);
+    expect(pieData.yield, 0.80);
   });
 }
