ad2d95d65b9c0d33a8a096f1ec94861c9b7f7991
melo <127689966+avocadotortilla@users.noreply.github.com>
2023-05-14 23:05:45 +0200

    change password

--- lib/views/ChangePasswordPage.dart
+++ lib/views/ChangePasswordPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mysql1/mysql1.dart';
 import '../styles.dart';
+import '../ConnectionSettings.dart';
+import '../Arguments/UserInfoArguments.dart';
 
 class ChangePassword extends StatefulWidget {
   const ChangePassword({super.key});
@@ -14,24 +17,70 @@ class _ChangePasswordState extends State<ChangePassword> {
   TextEditingController newPasswordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    final arguments = ModalRoute.of(context)!.settings.arguments as List;
+    //Extract the user's ID and gardens from the arguments
+    String email = arguments[2];
+
+    Future<MySqlConnection> getConnection() async {
+      var settings = ConnectionSettings(
+          host: 'db4free.net',
+          port: 3306,
+          user: 'hardcoded',
+          password: '5Scrummies@SD',
+          db: 'harvestapp');
+      return await MySqlConnection.connect(settings);
+    }
+
+    Future<String?> oldPasswordVerifier(String? pswd) async {
+      // Establish a connection to the database
+      final conn = await getConnection();
+      try {
+        // Execute a query to check if the password matches
+        final results = await conn.query(
+            'select user_password from USERS where user_email = ?',
+                          [email]);
+
+        // If the query returns exactly one row, the login was successful
+        if (results.toString() != pswd) {
+          return 'Password is not correct';
+        } else {
+          return null;
+        }
+      } catch (e) {
+        // Handle any exceptions that occur during the query execution
+        return null;
+      } finally {
+        // Close the connection when done
+        await conn.close();
+      }
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
         iconTheme: IconThemeData(color: secondaryColour),
+        title: Text(
+          'Change Password',
+          style: secondaryColourText,
+        ),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
                     "enter old password",
                     style: blackText.copyWith(
                         fontWeight: FontWeight.bold,
@@ -39,82 +88,63 @@ class _ChangePasswordState extends State<ChangePassword> {
                         fontSize: 14),
                     textAlign: TextAlign.left,
                   ),
-                  SizedBox(
-                    height: 2,
-                  ),
+                ),
 
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 6),
-                    child: TextFormField(
-                      obscureText: true,
-                      controller: oldPasswordController,
-                      decoration:  InputDecoration(
+                SizedBox(
+                  height: 2,
+                ),
+
+                //old password
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  height: 50,
+                  width: 275,
+                  child: TextFormField(
+                    obscureText: true,
+                    controller: oldPasswordController,
+                    decoration: InputDecoration(
                         enabledBorder: OutlineInputBorder(
                             borderSide:
                                 BorderSide(color: primaryColour, width: 1.5)),
                         focusedBorder: OutlineInputBorder(
                             borderSide:
                                 BorderSide(color: primaryColour, width: 1.5)),
-                        errorBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 1.5)),
-                        labelStyle: blackText
-                      ),
-                    ),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1.5)),
+                        labelStyle: blackText),
+                    validator: (value) {
+                         oldPasswordVerifier(value);
+                    },
                   ),
-
-                  SizedBox(height: 4,),
-            
-                  Text(
-                    "enter new password",
-                    style: blackText.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                      fontSize: 14),
-                  ),
-
-                  SizedBox(
-                    height: 2,
-                  ),
-
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 6),
-                    child: TextFormField(
-                      obscureText: true,
-                      controller: oldPasswordController,
-                      decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: primaryColour, width: 1.5)),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: primaryColour, width: 1.5)),
-                          errorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 1.5)),
-                          labelStyle: blackText),
-                    ),
-                  ),
-
-                  SizedBox(
-                    height: 4,
-                  ),
-            
-                  Text(
-                  "confirm new password",
-                  style: blackText.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                      fontSize: 14),
                 ),
 
                 SizedBox(
-                height: 2,
+                  height: 4,
                 ),
 
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "enter new password",
+                    style: blackText.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                        fontSize: 14),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 2,
+                ),
+
+                //new password
                 Container(
-                  padding: const EdgeInsets.only(bottom: 6),
+                  padding: const EdgeInsets.only(bottom: 10),
+                  height: 50,
+                  width: 275,
                   child: TextFormField(
                     obscureText: true,
-                    controller: oldPasswordController,
+                    controller: newPasswordController,
                     decoration: InputDecoration(
                         enabledBorder: OutlineInputBorder(
                             borderSide:
@@ -131,12 +161,110 @@ class _ChangePasswordState extends State<ChangePassword> {
                 SizedBox(
                   height: 4,
                 ),
-                
-                ],
-              ),
-            ]
+
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "confirm new password",
+                    style: blackText.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                        fontSize: 14),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 2,
+                ),
+
+
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  height: 50,
+                  width: 275,
+                  child: TextFormField(
+                    obscureText: true,
+                    controller: confirmPasswordController,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: primaryColour, width: 1.5)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: primaryColour, width: 1.5)),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1.5)),
+                        labelStyle: blackText),
+                        validator: (value) {
+                          if (value != newPasswordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          else {
+                            return null;
+                          }
+                        },
+                  ),
+                ),
+
+                SizedBox(
+                  height: 4,
+                ),
+
+                Container(
+                    height: 90,
+                    padding: const EdgeInsets.only(top: 40),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: secondaryColour,
+                        ),
+                        onPressed: () async {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            //If the form is valid
+                            //Establish connection with the database
+                            var conn = await MySqlConnection.connect(
+                                ConnectionSettings(
+                                    host: 'db4free.net',
+                                    port: 3306,
+                                    user: 'hardcoded',
+                                    password: '5Scrummies@SD',
+                                    db: 'harvestapp'));
+                            
+                            var password = newPasswordController.text;
+                            //Add the new user to the database (the USER table in the database needs to auto increment user_id for this command to work)
+                            try {
+                              // Prepare and execute the SQL statement
+                              final stmt = await conn.query(
+                                'UPDATE USERS SET user_password = ? WHERE user_email = ?',
+                                [password, email]
+                              );
+
+                              if (stmt.affectedRows == 1) {
+                                print('Value updated successfully');
+                                Navigator.pop(context);
+                              } else {
+                                print('Failed to update value');
+                              }
+                            } catch (e) {
+                              print('Error: $e');
+                            } finally {
+                              // Close the database connection
+                              await conn.close();
+                            }
+                          }
+                          //Trying to navigate back to the login page
+                          else if (_formKey.currentState!.validate()) {}
+                        },
+                        child: Text('Change Password',
+                            style: signUpPageText.copyWith(
+                              fontSize: 25,
+                              color: primaryColour,
+                            )))),
+              ],
+            ),
           ),
         ),
+      ),
     );
   }
 }
